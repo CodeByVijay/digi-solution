60923546367066a4aaa62d15683d450e3b112936
Vijay Amule <vijay.amule@brancosoft.com>
2022-05-27 18:38:34 +0530

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -145,8 +145,10 @@
                             <p class="section-title text-white h5 mb-4">Newsletter<span></span></p>
                             <p>Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulpu</p>
                             <div class="position-relative w-100 mt-3">
-                                <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="text" placeholder="Your Email" style="height: 48px;">
-                                <button type="button" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane text-primary fs-4"></i></button>
+                                <form action="" method="post">
+                                    <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="text" placeholder="Your Email" style="height: 48px;">
+                                    <button type="submit" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane text-primary fs-4"></i></button>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -155,8 +157,8 @@
                     <div class="copyright">
                         <div class="row">
                             <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                                &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-                                Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                                &copy; <a class="border-bottom" href="#">DG Solution</a>, All Right Reserved.
+                                Designed By <a class="border-bottom" href="#">DG Solution</a>
                             </div>
                             <div class="col-md-6 text-center text-md-end">
                                 <div class="footer-menu">
